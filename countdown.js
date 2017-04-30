02e5554049fab7f240b9e6e460a13e359f00aea5
zhenggangchen <458409991@qq.com>
2017-04-30 15:23:55 +0800

    基本完成欠缺优化

--- countdown.js
+++ countdown.js
@@ -8,6 +8,9 @@ var RADIUS = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 
+var balls = [];
+const colors = ["#33b5e5", "#0099cc", "#aa66cc", "#9933cc", "#99cc00", "#669900", "#ffbb33", "#ff8800", "#ff4444", "#cc0000"];
+
 
 //const endTime=new Date(2014,6,11,18,47,52);
 //const endTime=new Date(2014,6,11,18,47,52);
@@ -55,9 +58,63 @@ function update() {
     var curSeconds = curShowTimeSeconds % 60;
 
     if (nextSeconds != curSeconds) {
+
+        if (parseInt(curHours / 10) != parseInt(nextHours / 10)) {
+            addBalls(MARGIN_LEFT, MARGIN_TOP, parseInt(curHours / 10));
+        }
+        if (parseInt(curHours % 10) != parseInt(nextHours % 10)) {
+            addBalls(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(curHours / 10));
+        }
+        if (parseInt(curMinutes / 10) != parseInt(nextMinutes / 10)) {
+            addBalls(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(curMinutes / 10));
+        }
+        if (parseInt(curMinutes % 10) != parseInt(nextMinutes % 10)) {
+            addBalls(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(curMinutes % 10));
+        }
+        if (parseInt(curSeconds / 10) != parseInt(nextSeconds / 10)) {
+            addBalls(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(curSeconds / 10));
+        }
+        if (parseInt(curSeconds % 10) != parseInt(nextSeconds % 10)) {
+            addBalls(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(nextSeconds % 10));
+        }
+
+
         curShowTimeSeconds = nextShowTimeSeconds;
     }
+
+    updateBalls();
 }
+
+function updateBalls() {
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].x += balls[i].vx;
+        balls[i].y += balls[i].vy;
+        balls[i].vy += balls[i].g;
+
+        if (balls[i].y >= WINDOW_HEIGHT - RADIUS) {
+            balls[i].y = WINDOW_HEIGHT - RADIUS;
+            balls[i].vy = -balls[i].vy * 0.75;
+        }
+
+    }
+}
+
+function addBalls(x, y, num) {
+    for (var i = 0; i < digit[num].length; i++)
+        for (var j = 0; j < digit[num][i].length; j++)
+            if (digit[num][i][j] == 1) {
+                var aBall = {
+                    x: x + j * 2 * (RADIUS + 1) + (RADIUS + 1),
+                    y: y + i * 2 * (RADIUS + 1) + (RADIUS + 1),
+                    g: 1.5 + Math.random(),
+                    vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,
+                    vy: -5,
+                    color: colors[Math.floor(Math.random() * colors.length)]
+                };
+                balls.push(aBall);
+            }
+}
+
 
 function render(ctx) {
 
@@ -75,6 +132,14 @@ function render(ctx) {
     renderDigit(MARGIN_LEFT + 69 * (RADIUS + 1), MARGIN_TOP, 10, ctx);
     renderDigit(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(second / 10), ctx);
     renderDigit(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(second % 10), ctx);
+
+    for (var i = 0; i < balls.length; i++) {
+        ctx.fillStyle = balls[i].color;
+        ctx.beginPath();
+        ctx.arc(balls[i].x, balls[i].y, RADIUS, 0, 2 * Math.PI, true);
+        ctx.closePath();
+        ctx.fill();
+    }
 }
 function renderDigit(x, y, num, ctx) {
 
